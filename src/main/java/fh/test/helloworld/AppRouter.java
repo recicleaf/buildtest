1e70c44eb76bb2b95b49cfa02c59657763a44ddd
Felipe Hernandez <fllamas@travix.com>
2019-07-26 10:59:07 +0100

    feat: add environment dependency

--- src/main/java/fh/test/helloworld/AppRouter.java
+++ src/main/java/fh/test/helloworld/AppRouter.java
@@ -1,5 +1,6 @@
 package fh.test.helloworld;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RequestPredicates;
@@ -12,11 +13,14 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 @Configuration
 public class AppRouter {
 
+    @Value("${name}")
+    private String name;
+
     @Bean
     RouterFunction<ServerResponse> updateEmployeeRoute() {
         return RouterFunctions.route(RequestPredicates.GET("/hello"),
-                                     request -> ok().syncBody("Hello World!"))
+                                     request -> ok().syncBody(this.name + " World!"))
                               .andRoute(RequestPredicates.GET("/hello2"),
-                                        request -> ok().syncBody("Hello World 2!"));
+                                        request -> ok().syncBody(this.name + " World 2!"));
     }
 }
